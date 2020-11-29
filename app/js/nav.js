b3d1978c7d959c533151ba503f11a35af52d7444
koralina <kjedrzejak91@gmail.com>
2020-11-29 16:27:13 +0100

    changed function stucture

--- app/js/nav.js
+++ app/js/nav.js
@@ -1,11 +1,7 @@
-const burgerBtn = document.querySelector(".js-toggle-menu");
-const navList = document.querySelector(".js-nav-list");
+var burgerBtn = document.querySelector(".js-toggle-menu");
+var navList = document.querySelector(".js-nav-list");
 
-let toggle = () => {
+burgerBtn.addEventListener("click",function(){
     burgerBtn.classList.toggle("is_active");
     navList.classList.toggle("is_open");
-}
-
-burgerBtn.addEventListener('click', toggle);
-
-
+})
